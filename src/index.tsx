46bbc0e52e07b37ad2d2e780cc3744ff4763666d
Rason <58801635+rasonpang@users.noreply.github.com>
2024-05-01 22:24:34 +0800

    add routes

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,25 @@
 /* @refresh reload */
+import { lazy } from "solid-js";
 import { render } from "solid-js/web";
 import { Router, Route } from "@solidjs/router";
 
+// Global CSS
+import "./index.css";
+
+// Layout
 import BarLayout from "@/app/layouts/BarLayout";
-import HomePage from "@/app/routes";
+
+// Routes configuration
+const routes = [
+	{ path: "/", component: lazy(() => import("@/app/routes/index")) },
+];
 
 render(
 	() => (
 		<Router root={BarLayout}>
-			<Route path="/" component={HomePage} />
+			{routes.map(({ path, component }) => (
+				<Route path={path} component={component} />
+			))}
 		</Router>
 	),
 	document.getElementById("root") as HTMLElement
