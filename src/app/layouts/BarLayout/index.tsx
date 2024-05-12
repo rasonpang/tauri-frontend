98e6862238ccb0c4ce8d73d81364448b86a3add1
Rason <58801635+rasonpang@users.noreply.github.com>
2024-05-12 15:26:24 +0800

    design layout

--- src/app/layouts/BarLayout/index.tsx
+++ src/app/layouts/BarLayout/index.tsx
@@ -1,9 +1,28 @@
+import { createSignal } from "solid-js";
 import styles from "./style.module.css";
 
 const BarLayout = (props: any) => {
+	const [headerVisibility, setHeaderVisibility] = createSignal(true);
+
+	const onScroll = (e: any) => {
+		let scrollPos;
+		if (e.wheelDeltaY < 0) scrollPos = "down";
+		else if (e.wheelDeltaY > 0) scrollPos = "up";
+
+		setHeaderVisibility(scrollPos === "up" ? true : false);
+	};
+
+	window.addEventListener("wheel", onScroll);
+
 	return (
-		<div>
-			<div class={styles.header}></div>
+		<div class={styles.container}>
+			<div
+				class={`${styles.header} ${
+					headerVisibility() === false ? styles["header-hide"] : ""
+				}`}
+			>
+				Header
+			</div>
 			<div class={styles.content}>{props.children}</div>
 		</div>
 	);
--- src/app/routes/index.tsx
+++ src/app/routes/index.tsx
@@ -1,5 +1,80 @@
 const HomePage = () => {
-	return <div>Home Page</div>;
+	const test = `Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit
+	vero quod quisquam aliquid quae, soluta sequi modi consectetur nobis
+	ducimus accusantium. Debitis eveniet ipsum nostrum error modi.
+	Molestias, dolorem molestiae! Iusto, ipsa ut sed aliquam fugit
+	quaerat quam praesentium neque quae non tempora ipsum unde atque.
+	Optio facere dignissimos, commodi ab obcaecati iusto repellendus
+	ducimus vitae, autem ea tempore beatae. Eligendi rem voluptates
+	quisquam! Amet dolores dolor totam autem animi dolorem doloribus,
+	impedit quae. Consequatur, nemo vel, quo dolorem totam suscipit
+	laborum explicabo, tempore cumque laudantium molestiae illo a nam!
+	Laudantium iusto officia delectus placeat eveniet quaerat maiores
+	sunt magni cumque nesciunt soluta, et, cum molestias impedit
+	cupiditate ad dolor. Eveniet quisquam sequi quibusdam, temporibus
+	aperiam voluptatem fugiat ullam deserunt. Sint consectetur dolor
+	odit, optio atque iure blanditiis quasi ex neque quas quam veniam
+	libero fugit quia rerum laboriosam quis illo facere nulla. Dolorem,
+	quibusdam? Ut id quidem quod blanditiis. Aliquid labore, unde
+	similique, rem quidem fuga eius tempore quia alias suscipit
+	provident obcaecati modi id ipsam? Quo rerum quia magnam, tempore a
+	neque laudantium qui architecto incidunt, consequuntur sunt.
+	Laudantium similique quo omnis perspiciatis impedit asperiores
+	excepturi. Ratione, asperiores numquam cumque nostrum laborum
+	suscipit dolore cupiditate ut nobis adipisci aspernatur culpa
+	laudantium fugiat voluptatem illo totam dolorum quis explicabo.
+	Cumque, commodi aspernatur numquam distinctio sequi soluta fugit et
+	voluptatibus dicta aut nihil, quam cupiditate officiis ducimus
+	impedit, reiciendis veritatis voluptas tempore? Suscipit esse dicta,
+	quia veniam incidunt eius iusto. Quasi, aut! Earum dicta doloribus
+	possimus enim eaque quos! Delectus sunt laborum nostrum, aperiam
+	quae impedit nam atque, ipsum, nesciunt aliquam minus reprehenderit
+	itaque debitis eaque voluptates eveniet suscipit quo. Ducimus
+	quaerat cumque vel quae nobis dolorum quos, sit natus? Possimus iure
+	deleniti repellat fugit temporibus accusantium ipsa veritatis
+	distinctio, eum, veniam sed quia quo facilis, sunt praesentium
+	consequuntur a. Cumque ea beatae a optio facilis officiis assumenda
+	excepturi nostrum distinctio blanditiis consectetur, est vero
+	recusandae voluptates quas iusto placeat iste. Minima totam
+	accusamus dicta. Quis sit vero soluta aspernatur? Magnam aspernatur
+	doloribus sunt maiores, iusto et maxime quo officiis quod nostrum
+	aliquam at mollitia earum ipsa, in, tenetur ipsum? Expedita minima
+	voluptas id quo praesentium, omnis fuga consequatur iusto! Eius nemo
+	officia sunt velit, aliquam temporibus debitis qui soluta doloremque
+	accusantium consequuntur, unde sequi corporis inventore expedita
+	deserunt atque impedit? Unde excepturi nam dolores aliquam omnis
+	neque tenetur eveniet? Doloribus odit fugiat, reiciendis sapiente,
+	placeat et deserunt debitis saepe veniam illo temporibus quam
+	explicabo magnam id quo quis numquam aperiam unde. Totam excepturi
+	mollitia temporibus dolores esse ea deserunt. Ducimus hic fuga,
+	consectetur id nam provident est enim ut, labore eligendi, nihil
+	perferendis minus accusantium odit nesciunt illo in fugiat suscipit
+	corrupti dignissimos. Aut reiciendis consectetur sequi quos eius.
+	Similique dicta expedita obcaecati eligendi nam doloribus impedit
+	dolor mollitia ab maiores, distinctio omnis laboriosam placeat
+	facere, voluptatum accusamus earum alias eum quidem cumque quo
+	inventore. Id accusamus laborum corporis? Fuga dolores consequatur
+	sequi unde animi aliquam exercitationem voluptas tenetur repellat
+	voluptatem veritatis doloremque totam voluptatibus vel provident
+	laboriosam, autem illum a impedit possimus aut et beatae!
+	Cupiditate, sit dolores. Autem corrupti iure maiores soluta, placeat
+	exercitationem error quidem officia at officiis laboriosam.
+	Recusandae dolorum voluptatum eaque numquam rerum ipsam commodi
+	reiciendis nisi, tempore distinctio cum aperiam obcaecati
+	consequuntur eligendi! Velit odit consectetur, excepturi ipsa
+	veritatis eius iure, soluta beatae harum qui officia minus animi
+	doloribus nobis vero quaerat aspernatur repellendus. Alias harum
+	fuga recusandae mollitia. Eligendi vero deserunt quasi! Nulla
+	doloribus quae exercitationem nesciunt voluptates aliquid animi
+	aspernatur reiciendis est quam sunt libero culpa sequi ipsum
+	asperiores voluptatibus sint error accusantium, consequatur nam
+	delectus magnam veniam voluptas! Sunt, consequuntur.`;
+
+	return (
+		<div>
+			{test} {test}
+		</div>
+	);
 };
 
 export default HomePage;
